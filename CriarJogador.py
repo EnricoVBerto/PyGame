df6e7df8945ab32bbfbdd87f2fb8aa265dd345c3
EnricoVBerto <enricovberto@hotmail.com>
2024-05-15 09:03:25 -0300

    Separação 2.0

---
+++ CriarJogador.py
@@ -0,0 +1,5 @@
+from main import *
+player = Ship(ship_img, all_sprites, all_ataques, ataque_img)
+all_sprites.add(player)
+player2 = Ship(fighter_img, all_sprites, all_ataques, ataque_img)
+all_sprites.add(player2)
